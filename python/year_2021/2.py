c7003d7e9f0d423207109054aa15be2214951889
ben <bdenglish@gmail.com>
2021-12-02 01:10:30 -0500

    day 2 in python

--- python/year_2021/2.py
+++ python/year_2021/2.py
@@ -2,3 +2,37 @@ import common
 
 
 lines = common.read_input_lines_to_list(2021, 2)
+
+h = 0
+d = 0
+
+for line in lines:
+    cmd, dist = line.split()
+    if cmd == "forward":
+        h += int(dist)
+    elif cmd == "down":
+        d += int(dist)
+    elif cmd == "up":
+        d -= int(dist)
+
+print(h * d)
+
+h = 0
+d = 0
+aim = 0
+
+for line in lines:
+    cmd, dist = line.split()
+    dist = int(dist)
+    if cmd == "forward":
+        h += dist
+        d = d + (aim * dist)
+    elif cmd == "down":
+        aim += dist
+    elif cmd == "up":
+        aim -= dist
+    print(f'cmd: {cmd}, dist: {dist}')
+    print(f'h: {h}, d: {d}, aim: {aim}')
+    print('-')
+
+print(h * d)
